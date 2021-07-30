48add6111e783732aaebb074106fbd89e10d237b
Luke Primis <luke_primis@brown.edu>
2021-07-30 16:11:11 -0400

    completed fac.sol

--- contracts/Fac.sol
+++ contracts/Fac.sol
@@ -18,18 +18,27 @@ contract Factory {
         bytes32 tickerQ,
         bytes32 tickerT) external returns (address pair) {
         // todo: fill in the require conditions
-        require(?, 'First token in pair is not quote token');
-        require(?, 'Identical addresses');
+        require(tickerQ == "PIN", 'First token in pair is not quote token');
+        require(tokenA != tokenB, 'Identical addresses');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         uint whichP = tokenA < tokenB ? 1 : 2;
-        require(?, 'Zero address error');
-        require(?, 'Pair already exists');
+        require(tokenA != 0 && tokenB != 0 && dex != 0, 'Zero address error');
+        require(getPair[token0][token1] == 0 && getPair[token1][token0] == 0, 'Pair already exists');
         
         // todo: fill in the rest of the createPair method. To do this, you will need to deploy a smart contract using
         // a create2 opcode in assembly. This is probably beyond the scope of this project, so exact details will be
         // released on Piazza a few days after project release. Before that, you are encouraged to try your hand at
         // making the code yourself. You will also want to initialize the pool properly, and record the pair created 
         // properly in this contract.
+        bytes memory bytecode = type(Pool).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        Pool(pair).initialize(token0, token1, dex, whichP, tickerQ, tickerT);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair;
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
     }
-    
 }
